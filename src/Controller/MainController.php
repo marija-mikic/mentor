cd52a85fa16c3aacc94e3041037b2ce65c6d2afd
Marko Vušak <marko.vusak@real-digital.de>
2022-07-25 09:21:18 +0200

    Fix template rendering

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,16 +7,16 @@ namespace App\Controller;
  use Symfony\Component\HttpFoundation\Response;
  use Symfony\Component\Routing\Annotation\Route;
 
-class MainController
+final class MainController extends AbstractController
 {
-    /**
+	/**
      * @Route("/home", name="main")
      */
     #[Route]
     public function index(): Response
     {
-        echo $this->render('main', [
-            'content' => $this->view->render('index')
+        return $this->render('main/index.html.twig', [
+            'controller_name' => MainController::class
         ]);
     }
 }
