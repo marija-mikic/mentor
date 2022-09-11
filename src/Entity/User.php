6e3f685ec9cc36ce6d3d23f1fcebd88b87d59161
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-09-11 19:26:31 +0200

    make relations with State table

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -82,6 +82,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\ManyToOne()]
     private   $country = null;
 
+    #[ORM\ManyToOne(inversedBy: 'users')]
+    private ?state $state = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -151,9 +154,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @see UserInterface
      */
-<<<<<<< HEAD
-    public function eraseCredentials():void
-=======
     public function getSalt(): ?string
     {
         return null;
@@ -163,7 +163,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @see UserInterface
      */
     public function eraseCredentials()
->>>>>>> 82d901c (reset NewRegistration on main)
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
