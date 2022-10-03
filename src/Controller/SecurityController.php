cfa1ac286bff8b5939e229d89d072ad966d94ee4
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-10-04 00:32:51 +0200

    new commit after revert on newregistration
    pokušaj povlačenja commita te otvaranja novog brancha

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,8 +1,5 @@
 <?php
-
 declare(strict_types=1);
-
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,26 +12,21 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        if ($this->getUser()) {
-            $this->addFlash('warning', 'You are already logged in');
-          return $this->redirectToRoute('/');
-         }
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', 
-        [   'last_username' => $lastUsername, 
-            'error' => $error
-        ]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
-     
 }
