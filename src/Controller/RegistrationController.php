4acb5bd2006d14672db2324a9cec754e93903e2d
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-10-03 23:04:59 +0200

    refactor all  project
    Whole project rebuilt with new packages.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -1,23 +1,27 @@
 <?php
+
 declare(strict_types=1);
+
+
 
 
 namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Security\EmailVerifier;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class RegistrationController extends AbstractController
 {
-<<<<<<< HEAD
-    #[Route('/register', name: 'app_register')]
-=======
     private EmailVerifier $emailVerifier;
 
     public function __construct(EmailVerifier $emailVerifier)
@@ -34,7 +38,6 @@ class RegistrationController extends AbstractController
      * @param  mixed $entityManager
      * @return Response
      */
->>>>>>> b2bdceb (extensions PHPdoc)
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
@@ -52,13 +55,42 @@ class RegistrationController extends AbstractController
 
             $entityManager->persist($user);
             $entityManager->flush();
+
+            // generate a signed url and email it to the user
+            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+                (new TemplatedEmail())
+                    ->from(new Address('marija.snowml@gmail.com', 'marija'))
+                    ->to($user->getEmail())
+                    ->subject('Please Confirm your Email')
+                    ->htmlTemplate('registration/confirmation_email.html.twig')
+            );
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_main');
+            return $this->redirectToRoute('main');
         }
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+    }
+
+    #[Route('/verify/email', name: 'app_verify_email')]
+    public function verifyUserEmail(Request $request): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // validate email confirmation link, sets User::isVerified=true and persists
+        try {
+            $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
+        } catch (VerifyEmailExceptionInterface $exception) {
+            $this->addFlash('verify_email_error', $exception->getReason());
+
+            return $this->redirectToRoute('app_register');
+        }
+
+        // @TODO Change the redirect on success and handle or remove the flash message in your templates
+        $this->addFlash('success', 'Your email address has been verified.');
+
+        return $this->redirectToRoute('app_register');
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,60 +3,23 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
- 
+
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\Column()]
     private ?int $id = null;
-    
-    #[Assert\Length(
-        min: 1,
-        max: 5,
-        minMessage: 'Your first name must be at least {{ limit }} characters long',
-        maxMessage: 'Your first name cannot be longer than {{ limit }} characters',
-    )]
-    #[Assert\Type('string')]
-    private $name;
 
-     
-    #[Assert\NotBlank] 
-    private $adress;
-
-    #[Assert\NotBlank]
-    #[Assert\Length(max: 5)]
-    private $house_number;
-
-    #[Assert\Type('integer')]
-    #[Assert\Length(
-        min: 1,
-        max: 5,
-        minMessage: 'Post number must be at least {{ limit }} characters long',
-        maxMessage: 'Post number cannot be longer than {{ limit }} characters',
-    )]
-    private $post_number;
-
-    #[Assert\Type('string')]
-    #[Assert\Length(
-        min: 2,     
-        minMessage: 'City must be at least {{ limit }} characters long',
-        
-    )]
-    private $city;
-    
-  	#[ORM\Column(length: 180, unique: true)]
-    #[Assert\Email(
-        message: 'The email {{ value }} is not a valid email.',
-    )]
-    #[Assert\Type(Address::class)]
+    #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
 
     #[ORM\Column]
@@ -65,7 +28,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private array $roles = [];
 
-       /**
+    /**
      * @var string The hashed password
      */
     #[ORM\Column]
@@ -87,7 +50,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         minMessage: 'Your first name must be at least {{ limit }} characters long',
         maxMessage: 'Your first name cannot be longer than {{ limit }} characters',
         )]
-    private ?string $surname = null;
+    private ?string $lastname = null;
 
     #[ORM\Column]
     #[Assert\Regex(
@@ -106,23 +69,26 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         max: 5,
         maxMessage: 'Ymax {{ limit }}',
     )]
-    private ?string $postcode = null;
+    private ?int $postcode = null;
 
 
     #[Assert\NotBlank]
     #[ORM\Column(length: 255)]
     private ?string $city = null;
 
-    #[ORM\ManyToOne]
-    private ?Country $country = null;
+    #[ORM\Column]
+    private array $roles = [];
 
     #[ORM\Column]
-    private ?int $house_number = null;
+    private ?string $password = null;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
 
     #[ORM\ManyToOne(inversedBy: 'users')]
     private ?state $state = null;
 
-       public function getId(): ?int
+    public function getId(): ?int
     {
         return $this->id;
     }
@@ -149,9 +115,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return (string) $this->email;
     }
 
-    public function getSalt(): ?string
+    /**
+     * @deprecated since Symfony 5.3, use getUserIdentifier instead
+     */
+    public function getUsername(): string
     {
-        return null;
+        return (string) $this->email;
     }
 
     /**
@@ -189,106 +158,33 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     }
 
     /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
      * @see UserInterface
      */
-    public function eraseCredentials():void
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
 
-    public function getName(): ?string
+    public function isVerified(): bool
     {
-        return $this->name;
+        return $this->isVerified;
     }
 
-    public function setName(string $name): self
+    public function setIsVerified(bool $isVerified): self
     {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getSurname(): ?string
-    {
-        return $this-> surname;
-    }
-
-    public function setSurname(string $surname): self
-    {
-        $this-> surname = $surname;
-
-        return $this;
-    }
-
-    public function getUsername(): ?string
-    {
-        return $this->username;
-    }
-
-    public function setUsername(string $username): self
-    {
-        $this->username = $username;
-
-        return $this;
-    }
-
-    public function getAdress(): ?string
-    {
-        return $this->adress;
-    }
-
-    public function setAdress(string $adress): self
-    {
-        $this->adress = $adress;
-
-        return $this;
-    }
-
-    public function getPostcode(): ?int
-    {
-        return $this->postcode;
-    }
-
-    public function setPostcode(int $postcode): self
-    {
-        $this->postcode = $postcode;
-
-        return $this;
-    }
-
-    public function getCity(): ?string
-    {
-        return $this->city;
-    }
-
-    public function setCity(string $city): self
-    {
-        $this->city = $city;
-
-        return $this;
-    }
-
-    public function getCountry(): ?country
-    {
-        return $this->country;
-    }
-
-    public function setCountry(?country $country): self
-    {
-        $this->country = $country;
-
-        return $this;
-    }
-
-    public function getHouseNumber(): ?int
-    {
-        return $this->house_number;
-    }
-
-    public function setHouseNumber(int $house_number): self
-    {
-        $this->house_number = $house_number;
+        $this->isVerified = $isVerified;
 
         return $this;
     }
@@ -305,4 +201,126 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
+    
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of surname
+     */ 
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * Set the value of surname
+     *
+     * @return  self
+     */ 
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of adress
+     */ 
+    public function getAdress()
+    {
+        return $this->adress;
+    }
+
+    /**
+     * Set the value of adress
+     *
+     * @return  self
+     */ 
+    public function setAdress($adress)
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of house_number
+     */ 
+    public function getHouse_number()
+    {
+        return $this->house_number;
+    }
+
+    /**
+     * Set the value of house_number
+     *
+     * @return  self
+     */ 
+    public function setHouse_number($house_number)
+    {
+        $this->house_number = $house_number;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of post_number
+     */ 
+    public function getPost_number()
+    {
+        return $this->post_number;
+    }
+
+    /**
+     * Set the value of post_number
+     *
+     * @return  self
+     */ 
+    public function setPost_number($post_number)
+    {
+        $this->post_number = $post_number;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of city
+     */ 
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set the value of city
+     *
+     * @return  self
+     */ 
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function isIsVerified(): ?bool
+    {
+        return $this->isVerified;
+    }
 }
