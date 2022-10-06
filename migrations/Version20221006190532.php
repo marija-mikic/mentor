7295424dba4cef80682744c57ec9172b9ea70332
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-10-07 00:33:39 +0200

    phpstan

---
+++ migrations/Version20221006190532.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221006190532 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE country (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, country_id INT DEFAULT NULL, house_number INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), INDEX IDX_8D93D649F92F3E70 (country_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE `user` ADD CONSTRAINT FK_8D93D649F92F3E70 FOREIGN KEY (country_id) REFERENCES country (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        
+    }
+}
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,51 +2,79 @@
 
 namespace App\Form;
 
+use App\Entity\Country;
 use App\Entity\State;
 use App\Entity\User;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-
+use Symfony\Component\Validator\Constraints\IsTrue;
+use ZipArchive;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('name', TextType::class, [
+            ->add('name', TextType::class, [ 
+                'required'=> false,                                  
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter your name',
+                        'message' => 'Please enter your name',                        
                     ]),
-                    new Length([
-                        'min' => 1,
-                        'minMessage' => 'Your name  should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 30,
-                    ]),
+                    new Length ([
+                        'min'=> 3,
+                        'minMessage'=> 'Your name should be at least {{ limit }} characters'
+                         
+                    ])
                 ]
             ])
-            ->add('surname', TextType::class)
-            ->add('address', Address::class)
-            ->add('house_number')
-            ->add('post_number')
-            ->add('city')
-            ->add('state', State::class, [
-               'placeholder'=> 'Choose State',
-               'choice_lebel'=> 'name'              
-
+            ->add('surname', TextType::class, [                 
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your name'
+                    ]),
+                    new Length ([
+                        'min'=> 3,
+                        'minMessage'=> 'Your name should be at least {{ limit }} characters'
+                         
+                    ])
+                ]
             ])
+            ->add('username')
+            ->add('adress')
+            ->add('house_number',NumberType::class,[
+                'mapped' => false
+            ])
+            ->add('postcode',NumberType::class,[
+                'mapped' => false
+            ])
+            ->add('city',TextType::class, [                 
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'name of city'
+                    ]),
+                    new Length ([
+                        'min'=> 2,
+                        'minMessage'=> 'Your name should be at least {{ limit }} characters'
+                         
+                    ])
+                ]
+            ])
+            ->add('country', EntityType::class, [
+                'class' => Country::class,
+                'placeholder'=> 'Choose country'
+            ])
+            ->add('email', EmailType::class)
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -65,7 +93,7 @@ class RegistrationFormType extends AbstractType
                         'message' => 'Please enter a password',
                     ]),
                     new Length([
-                        'min' => 6,
+                        'min' => 8,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
