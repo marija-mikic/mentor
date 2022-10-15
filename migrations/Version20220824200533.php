c78c9890c4495a6134d222b9c002aaa616591832
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-10-15 17:14:35 +0200

    reconstruction migration

--- migrations/Version20220824200533.php
+++ migrations/Version20220824200533.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221006231907 extends AbstractMigration
+final class Version20220824200533 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,11 +20,11 @@ final class Version20221006231907 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD name LONGTEXT NOT NULL, ADD surname LONGTEXT NOT NULL, ADD username VARCHAR(255) NOT NULL, ADD adress VARCHAR(255) NOT NULL, ADD postcode INT NOT NULL, ADD city VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-       
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `user` ADD name VARCHAR(30) DEFAULT NULL, ADD surname VARCHAR(30) NOT NULL, ADD adress VARCHAR(255) NOT NULL,  ADD postcode INT NOT NULL, ADD city VARCHAR(255) NOT NULL, ADD country VARCHAR(255) NOT NULL');
     }
 }
