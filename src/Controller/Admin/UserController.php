cbdedb81cb0ebacc94bc2d43d107d7d33cc79300
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-11-09 22:08:41 +0100

    edit user

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -2,11 +2,16 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserController extends AbstractController
 {
@@ -17,5 +22,33 @@ class UserController extends AbstractController
         return $this->render('dashboard/user/index.html.twig', [
             'users'=>$userRepository->findAll(),
         ]);
+    }
+     #[Route('/{id}/edit', name:'user_edit', methods:'GET')]
+    public function edit( Request $request, User $user, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager) 
+    {
+            $form = $this->createForm(RegistrationFormType::class, $user);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                // encode the plain password
+                $user->setPassword(
+                    $userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('app.user');
+            }
+            return $this->render('dashboard/user/edit.html.twig',[
+                'user' => $user,
+                'form' => $form->createView(),
+            ]);
+
+        
+
     }
 }
