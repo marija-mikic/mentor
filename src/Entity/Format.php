2459ab7db77f0e19c0739f0cd28fd9a04fdef024
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-11-22 21:26:03 +0100

    add view and add new format

---
+++ src/Entity/Format.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FormatRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FormatRepository::class)]
+class Format
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $height = null;
+
+    #[ORM\Column]
+    private ?int $widht = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getHeight(): ?int
+    {
+        return $this->height;
+    }
+
+    public function setHeight(int $height): self
+    {
+        $this->height = $height;
+
+        return $this;
+    }
+
+    public function getWidht(): ?int
+    {
+        return $this->widht;
+    }
+
+    public function setWidht(int $widht): self
+    {
+        $this->widht = $widht;
+
+        return $this;
+    }
+}
---
+++ src/Form/FormatFormType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Format;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormatFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'attr' => array(
+                    'class' => 'bg-transparent block border-b-2 w-full h-20 text-6xl outline-none',
+                    'placeholder' => 'Enter name...',
+                ),
+                'label' => false,
+                'required' => false
+            ])
+            ->add('height', IntegerType::class, [
+                'attr' => array(
+                    'class' => 'bg-transparent block mt-10 border-b-2 w-full h-20 text-6xl outline-none',
+                    'placeholder' => 'Enter heigh...'
+                ),
+                'label' => false,
+                'required' => false
+            ])
+            ->add('widht', IntegerType::class, [
+                'attr' => array(
+                    'class' => 'bg-transparent block mt-10 border-b-2 w-full h-20 text-6xl outline-none',
+                    'placeholder' => 'Enter widht...'
+                ),
+                'label' => false,
+                'required' => false
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Format::class,
+        ]);
+    }
+}
