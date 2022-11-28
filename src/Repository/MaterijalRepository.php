7672858e34ebceebd9b374f7ea097f9ce0b1f28f
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-11-28 18:27:10 +0100

    delete

--- src/Repository/MaterijalRepository.php
+++ src/Repository/MaterijalRepository.php
@@ -38,29 +38,4 @@ class MaterijalRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-
-//    /**
-//     * @return Materijal[] Returns an array of Materijal objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('m')
-//            ->andWhere('m.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('m.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Materijal
-//    {
-//        return $this->createQueryBuilder('m')
-//            ->andWhere('m.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
